3e02bcf3fea7883353706f18ba87b47990b0c76c
TheTinkerersHaven <93077613+TheTinkerersHaven@users.noreply.github.com>
2026-05-15 18:56:24 +0200

    feat(deploy): add deploy.yml workflow and change base path
    feat(vite.config): configured base path to use process.env.NODE_ENV for production/development environments

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/PromessiLink/',
+  base: process.env.NODE_ENV === 'production' ? '/PromessiLink/' : '/',
   server: {
     allowedHosts: ['unshaven-waggle-slate.ngrok-free.dev']
   }
