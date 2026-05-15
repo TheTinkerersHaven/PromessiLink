70a5c982f1db583d93dd3bc7a726d202981beb3d
TheTinkerersHaven <93077613+TheTinkerersHaven@users.noreply.github.com>
2026-05-15 18:40:31 +0200

    Aggiornato il percorso base nella configurazione di Vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/PromessiLink/',
   server: {
     allowedHosts: ['unshaven-waggle-slate.ngrok-free.dev']
   }
